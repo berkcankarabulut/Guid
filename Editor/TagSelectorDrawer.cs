bc8c60b3a0853072000c9b69b185b26d639ab8f8
Berkcan Karabulut <46574515+berkcankarabulut@users.noreply.github.com>
2025-03-11 11:21:52 +0300

    Update TagSelectorDrawer.cs

--- Editor/TagSelectorDrawer.cs
+++ Editor/TagSelectorDrawer.cs
@@ -1,28 +1,24 @@
+using System;
 using UnityEngine;
 using UnityEditor;
 using System.Collections.Generic;
 
 namespace Project.Utility.Editor
-{ 
-
-    [CustomPropertyDrawer(typeof(TagSelectorAttribute))]
+{  
     public class TagSelectorDrawer : PropertyDrawer
     {
         public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
         {
             if (property.propertyType == SerializedPropertyType.String)
-            {
-                // Unity’den tag listesini al
+            { 
                 string[] tags = UnityEditorInternal.InternalEditorUtility.tags;
                 int index = Mathf.Max(0, System.Array.IndexOf(tags, property.stringValue));
-
-                // Dropdown menü
+ 
                 index = EditorGUI.Popup(position, label.text, index, tags);
                 property.stringValue = tags[index];
             }
             else if (property.propertyType == SerializedPropertyType.Generic && property.isArray) 
-            {
-                // Eğer `List<string>` ise tag array'ini yönetelim
+            { 
                 EditorGUI.LabelField(position, label);
                 property.arraySize = EditorGUI.IntField(new Rect(position.x + 100, position.y, 50, position.height), property.arraySize);
 
