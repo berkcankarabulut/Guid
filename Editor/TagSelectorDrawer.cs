1b7c8e80df86888b746afebea3bd161e286754bd
Berkcan Karabulut <46574515+berkcankarabulut@users.noreply.github.com>
2025-03-11 11:16:11 +0300

    feat: Add TagSelector attribute for cleaner tag selection in Inspector
    Description:
    
    * Added TagSelector attribute, enabling Unity tag selection menu for both string and List<string>.
    * Implemented TagSelectorDrawer to provide a dropdown list in the Inspector.
    * InteractableBase now has a more user-friendly interface for requiredTags.

---
+++ Editor/TagSelectorDrawer.cs
@@ -0,0 +1,59 @@
+using UnityEngine;
+using UnityEditor;
+using System.Collections.Generic;
+
+namespace Project.Utility.Editor
+{ 
+
+    [CustomPropertyDrawer(typeof(TagSelectorAttribute))]
+    public class TagSelectorDrawer : PropertyDrawer
+    {
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            if (property.propertyType == SerializedPropertyType.String)
+            {
+                // Unity’den tag listesini al
+                string[] tags = UnityEditorInternal.InternalEditorUtility.tags;
+                int index = Mathf.Max(0, System.Array.IndexOf(tags, property.stringValue));
+
+                // Dropdown menü
+                index = EditorGUI.Popup(position, label.text, index, tags);
+                property.stringValue = tags[index];
+            }
+            else if (property.propertyType == SerializedPropertyType.Generic && property.isArray) 
+            {
+                // Eğer `List<string>` ise tag array'ini yönetelim
+                EditorGUI.LabelField(position, label);
+                property.arraySize = EditorGUI.IntField(new Rect(position.x + 100, position.y, 50, position.height), property.arraySize);
+
+                for (int i = 0; i < property.arraySize; i++)
+                {
+                    SerializedProperty element = property.GetArrayElementAtIndex(i);
+                    position.y += EditorGUIUtility.singleLineHeight;
+                    
+                    if (element.propertyType == SerializedPropertyType.String)
+                    {
+                        string[] tags = UnityEditorInternal.InternalEditorUtility.tags;
+                        int index = Mathf.Max(0, System.Array.IndexOf(tags, element.stringValue));
+                        index = EditorGUI.Popup(position, $"Tag {i + 1}", index, tags);
+                        element.stringValue = tags[index];
+                    }
+                }
+            }
+            else
+            {
+                EditorGUI.PropertyField(position, property, label);
+            }
+        }
+
+        public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+        {
+            if (property.isArray)
+            {
+                return (property.arraySize + 1) * EditorGUIUtility.singleLineHeight;
+            }
+            return EditorGUIUtility.singleLineHeight;
+        }
+    }
+
+}
